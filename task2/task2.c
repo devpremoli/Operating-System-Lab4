7102bdb48b41555dd0391e17f8b0aaaf40ab579b
Prem Oli <prem.oli@bison.howard.edu>
2023-11-27 16:50:35 -0500

    Lab 4 corrected

--- task2/task2.c
+++ task2/task2.c
@@ -11,41 +11,41 @@ int exit_program(int a, int b);
 
 int main (void)
 {
-	/* IMPLEMENT ME: Insert your algorithm here */
+    /* IMPLEMENT ME: Insert your algorithm here */
 	int a = 10;
-  int b = 5;
+    int b = 5;
 
 	OP operators[5] = {&add, &subtract, &multiply, &divide, &exit_program};
 	char idx[3];
 
 	while (1) {
-		printf("Operand 'a' : %d | Operand 'b' : %d\nSpecify the operation to perform (0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4 : exit): ", a, b);
+        printf("Operand 'a' : %d | Operand 'b' : %d\nSpecify the operation to perform (0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4 : exit): ", a, b);
 		fgets(idx, 3, stdin);
 		printf("x = %d\n", operators[(int)idx[0]-'0'](a, b));
 	}
-	
-  return 0;  
+
+    return 0;
 }
 
 /* IMPLEMENT ME: Define your functions here */
 int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
 
 int subtract(int a, int b) {
-	printf ("Subtracting 'a' and 'b'\n");
-  return a - b;
+    printf ("Subtracting 'a' and 'b'\n");
+    return a - b;
 }
 
 int multiply(int a, int b) {
-	printf ("Multiplying 'a' and 'b'\n");
-  return a * b;
+    printf ("Multiplying 'a' and 'b'\n");
+    return a * b;
 }
 
 int divide(int a, int b) {
-	if (b != 0) {
-		printf ("Dividing 'a' and 'b'\n");
+    if (b != 0) {
+        printf ("Dividing 'a' and 'b'\n");
 		return a / b;
 	} else {
-		printf("Division by zero is not allowed.\n");
+        printf("Division by zero is not allowed.\n");
 		return 0;
 	}
 }
